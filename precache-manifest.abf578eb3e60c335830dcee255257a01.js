74c5aa4328ff6d90b13ab395d651471e067dd104
David <dph0718@gmail.com>
2018-11-27 14:43:49 -0700

    Updates

---
+++ precache-manifest.abf578eb3e60c335830dcee255257a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c22e95fac8b423985b8",
+    "url": "/my-site/static/js/runtime~main.5c22e95f.js"
+  },
+  {
+    "revision": "eb5dc973c3684d33a9fa",
+    "url": "/my-site/static/js/main.eb5dc973.chunk.js"
+  },
+  {
+    "revision": "8bdf941ce9aa1420caf4",
+    "url": "/my-site/static/js/1.8bdf941c.chunk.js"
+  },
+  {
+    "revision": "eb5dc973c3684d33a9fa",
+    "url": "/my-site/static/css/main.81bf34d3.chunk.css"
+  },
+  {
+    "revision": "74cb656b68e6a2026458afbd5d49f4b1",
+    "url": "/my-site/index.html"
+  }
+];
